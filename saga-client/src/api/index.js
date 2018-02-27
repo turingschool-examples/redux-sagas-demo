6fca09971a1329c16cba13c8ab70efd4a51416c5
Will Mitchell <wvmitchell@gmail.com>
2018-02-27 09:25:14 -0700

    add http

--- saga-client/src/api/index.js
+++ saga-client/src/api/index.js
@@ -1,6 +1,6 @@
 export const postLoginUser = async (email, password) => {
   try {
-    const response = await fetch('localhost:3000/users', {
+    const response = await fetch('http://localhost:3000/users', {
       method: 'POST',
       body: JSON.stringify({email, password}),
       headers: {
