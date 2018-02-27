31f5e35e581ba8cff04aba5790b61699dce63ab4
Will Mitchell <wvmitchell@gmail.com>
2018-02-27 09:18:26 -0700

    fix syntax error

--- saga-client/src/api/index.js
+++ saga-client/src/api/index.js
@@ -2,7 +2,7 @@ export const postLoginUser = async (email, password) => {
   try {
     const response = await fetch('localhost:3000/users', {
       method: 'POST',
-      body: JSON.stringify({email, password})
+      body: JSON.stringify({email, password}),
       headers: {
         "Content-Type": "application/json"
       }
