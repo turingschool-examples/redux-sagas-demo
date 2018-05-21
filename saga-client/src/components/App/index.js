95b926a35e3fdf280dd2daed5f4c9ec0a3c503a0
Will Mitchell <wvmitchell@gmail.com>
2018-05-21 06:53:45 -0600

    move store creation up the project

--- saga-client/src/components/App/index.js
+++ saga-client/src/components/App/index.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
 import { Route, Redirect } from 'react-router';
 
 import Login from '../../containers/Login'
 import Main from '../../containers/Main'
-import rootReducer from '../../reducers';
 import './styles.css';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <Route path='/' render={() => <Redirect to='/login' />} />
-            <Route path='/login' component={Login} />
-            <Route path='/main' component={Main} />
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <div>
+        <Route path='/' render={() => <Redirect to='/login' />} />
+        <Route path='/login' component={Login} />
+        <Route path='/main' component={Main} />
+      </div>
     );
   }
 }
--- saga-client/src/index.js
+++ saga-client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const app = <Provider store={store}>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </Provider>
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
