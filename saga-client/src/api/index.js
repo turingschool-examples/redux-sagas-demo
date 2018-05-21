5b5feaec96e2278fc4d1e170e1bb7fbcafa85a9f
Will Mitchell <wvmitchell@gmail.com>
2018-05-21 06:54:10 -0600

    remove try/catch

--- saga-client/src/api/index.js
+++ saga-client/src/api/index.js
@@ -1,19 +1,15 @@
 export const postLoginUser = async (email, password) => {
-  try {
-    const response = await fetch('http://localhost:3000/users', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-
-    if(response.status === 200) {
-      return await response.json()
-    } else {
-      throw(new Error(`Errored with status code ${response.status}`))
+  const response = await fetch('http://localhost:3000/users', {
+    method: 'POST',
+    body: JSON.stringify({email, password}),
+    headers: {
+      "Content-Type": "application/json"
     }
-  } catch (err) {
-    throw(err)
+  })
+
+  if(response.status === 200) {
+    return await response.json()
+  } else {
+    throw(new Error(`Errored with status code ${response.status}`))
   }
 }
